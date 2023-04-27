3dcc0d8bfaadb5e8607a8c3e39fa34f19f3c2539
swapnach31 <131398332+swapnach31@users.noreply.github.com>
2023-04-27 14:43:03 +0000

    new changes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+
+import { ProductService } from './product.service'
 
 @NgModule({
   declarations: [
@@ -18,6 +21,7 @@ import { ProductPageComponent } from './product-page/product-page.component';
     HttpModule
   ],
   providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
